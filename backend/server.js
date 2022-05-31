132c42de7902a4a79d67c317869e8e3ae4c96a1a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2022-05-31 23:58:30 +0530

    mongodb connected to node.js server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 const app = express();
+
 dotenv.config();
+connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
@@ -19,4 +23,4 @@ app.get("/api/chat/:id", (req, res) => {
 	res.send(singleChat);
 });
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server started on port ${PORT}`.yellow.bold));

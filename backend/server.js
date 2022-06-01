cdb8fa4cf113cbcb81a5c427b3ce3c0cbd25bc84
ashmeetsmonga <ashmeetsmonga@gmail.com>
2022-06-01 21:22:32 +0530

    added registration api

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,16 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+app.use(express.json());
 
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-	res.send("API is running");
-});
-
-app.get("/api/chat", (req, res) => {
-	res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
 app.get("/api/chat/:id", (req, res) => {
 	const singleChat = chats.find((c) => c._id === req.params.id);
